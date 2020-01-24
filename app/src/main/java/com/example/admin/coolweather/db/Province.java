4aadd7baf7d28fb28a575edbbba1d6ec6675f9de
lansuqi <1421119874@qq.com>
2020-01-24 20:06:59 +0800

    加入数据库和表的各项配置.

---
+++ app/src/main/java/com/example/admin/coolweather/db/Province.java
@@ -0,0 +1,29 @@
+package com.example.admin.coolweather.db;
+
+/**
+ * Created by admin on 2020/1/24.
+ */
+
+public class Province {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getProvinceName() {
+        return provinceName;
+    }
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
